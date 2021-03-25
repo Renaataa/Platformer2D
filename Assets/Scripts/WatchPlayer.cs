1ba19d98144d7a13002e565da77087776c6a6152
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-03-25 18:10:27 +0100

    Smooth camera

---
+++ Assets/Scripts/WatchPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchPlayer : MonoBehaviour
+{
+    Transform player; 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime);
+    }
+}
