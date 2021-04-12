bef2f3fcfd36d0f9d36f88acd9c4f7b19eea1546
Renaataa <49534519+Renaataa@users.noreply.github.com>
2021-04-12 21:45:19 +0200

    Adding animation "kick" and "punch"

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -6,6 +6,7 @@ public class CharacterAnimation : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    int hit;
 
     void Start(){
         anim = GetComponent<Animator>();
@@ -15,7 +16,6 @@ public class CharacterAnimation : MonoBehaviour
     void Update(){
 
         if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))){
-            CrouchOff();
             anim.SetBool("isWalking", true);
         } else {
             anim.SetBool("isWalking", false);
@@ -23,7 +23,7 @@ public class CharacterAnimation : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.UpArrow) && GetComponent<PlayerController2>().isGrounded == true){
             if(!Input.GetKeyDown(KeyCode.DownArrow) && !Input.GetKey(KeyCode.DownArrow)){
-                CrouchOff();
+                HitOff();
                 anim.SetTrigger("jump");
             }
         }
@@ -38,13 +38,30 @@ public class CharacterAnimation : MonoBehaviour
             rb.constraints = RigidbodyConstraints2D.FreezePositionX|RigidbodyConstraints2D.FreezePositionY|RigidbodyConstraints2D.FreezeRotation;
 
             Invoke("CrouchOff", 0.35f);
-        } 
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && GetComponent<PlayerController2>().isGrounded == true){
+            hit = Random.Range(2, 3);
+
+            anim.SetInteger("hit", hit);
+            anim.SetInteger("hit", hit);
+
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX|RigidbodyConstraints2D.FreezePositionY|RigidbodyConstraints2D.FreezeRotation;
+
+            Invoke("HitOff", 0.6f);
+        } else {
+            anim.SetInteger("hit", -1);
+        }
     }
 
     void CrouchOff(){
         GetComponent<CapsuleCollider2D>().offset = new Vector2(-0.005184233f, -0.07877457f);
         GetComponent<CapsuleCollider2D>().size = new Vector2(0.2300652f, 0.4508078f);
 
+        rb.constraints = RigidbodyConstraints2D.None|RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void HitOff(){
         rb.constraints = RigidbodyConstraints2D.None|RigidbodyConstraints2D.FreezeRotation;
     }
 }
